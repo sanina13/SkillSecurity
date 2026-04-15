25a9af68f4f25855ec7e688428fc9020a0d06d40
Tiago Sanina <tiago.sanina13@gmail.com>
2026-04-15 21:00:28 +0100

    test2 passed

--- backend/scanner/detector.py
+++ backend/scanner/detector.py
@@ -1,2 +1,17 @@
+import re
+
 def scan(text):
-    return []
+    findings = []
+    overrideFind = override_attempt(text)
+    if not overrideFind is None:
+        findings.append(overrideFind)
+
+    return findings
+
+def override_attempt(text):
+    text_lower = text.lower()
+    if re.search(r"you\s+are\s+now\s+a", text_lower):
+        return{
+            "rule": "role-override", "severity": "critical"
+        }
+    return None
